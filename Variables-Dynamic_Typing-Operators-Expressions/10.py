5809b0ebfdfe4441f67502dbc23503c02e01373f
Kuldeep Chouhan <141010188+KuldeepC7@users.noreply.github.com>
2026-03-01 23:17:18 +0530

    Attempted Bitwise operator questions

--- Variables-Dynamic_Typing-Operators-Expressions/10.py
+++ Variables-Dynamic_Typing-Operators-Expressions/10.py
@@ -1,3 +1,6 @@
 # 10. You have a base configuration value of 2 (binary 0010). Use a bitwise left shift operator to quickly multiply this value by 8 to set a new system permission level.
 
 
+base_configuration_value = 2
+new_system_permission = 2 << 8
+print(new_system_permission)
--- Variables-Dynamic_Typing-Operators-Expressions/13.py
+++ Variables-Dynamic_Typing-Operators-Expressions/13.py
@@ -1,3 +1,8 @@
 # 13. A basic cryptography hashing function requires XORing a user's pin (1234) with a secret backend salt (5678). Write the expression to compute this bitwise XOR and store the ciphertext.
 
 
+user_pin = 1234
+secret_backend_salt = 5678
+
+ciphertext = user_pin ^ secret_backend_salt
+print(ciphertext)
